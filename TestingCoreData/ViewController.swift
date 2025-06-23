2e26026bf2d79f956cc254289a38918df3c67bea
prakharCreativeFrenzy <prakhar_dixit@creativefrenzy.in>
2025-06-23 12:25:21 +0530

    Initial Commit

---
+++ TestingCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingCoreData
+//
+//  Created by Creative Frenzy on 23/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
